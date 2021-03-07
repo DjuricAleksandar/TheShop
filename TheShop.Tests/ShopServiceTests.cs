bf6cafcc4c1e9f88ffd48b8142252f408925ec09
DjuricAleksandar <aleksandar.z.djuric@gmail.com>
2021-03-07 02:52:04 +0100

    Add tests for GetById method

--- TheShop.Tests/ShopServiceTests.cs
+++ TheShop.Tests/ShopServiceTests.cs
@@ -5,10 +5,25 @@ namespace TheShop.Tests
 {
 	public class ShopServiceTests
 	{
-		[Fact]
-		public void Test1()
-		{
+		private readonly ShopService _shop = new ShopService();
 
+		[Fact]
+		public void GetByIdNegativeValueReturnsErrorMessage()
+		{
+			const int id = -1;
+			var result = _shop.GetById(id);
+			Assert.True(result.IsError);
+			Assert.Equal(string.Format(Messages.GetByIdException, id), result.Message);
+		}
+
+		[Fact]
+		public void GetByIdExistingIdArticleReturnedSuccessfully()
+		{
+			const int id = 1;
+			var result = _shop.GetById(id);
+			Assert.False(result.IsError);
+			Assert.Empty(result.Message);
+			Assert.Equal(id, result.Result.ID);
 		}
 	}
 }
--- TheShop/Properties/AssemblyInfo.cs
+++ TheShop/Properties/AssemblyInfo.cs
@@ -34,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Add access to internal objects for testing.
+[assembly: InternalsVisibleTo("TheShop.Tests")]
